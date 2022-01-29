a9b4e9d22e0c12bf407f117c6cb8e0744cd96488
pickleprat <90265744+pickleprat@users.noreply.github.com>
2022-01-29 20:51:15 +0530

    Updating the assignment with a sort function of my own

--- sort_the_numbers.py
+++ sort_the_numbers.py
@@ -8,5 +8,17 @@ def number_sorting(*iterable: tuple):
     iterabled.sort()
     return iterabled 
 
+def anamize(collection: list):
+    sorted_list = list()
+    for i in range(len(collection)):
+        smallest = min(collection)
+        sorted_list.append(smallest)
+        collection.remove(smallest)
+    return sorted_list
+
+a_list = [6, 6, 2, 6, 0, 5]
+a_list = anamize(a_list)
+print(a_list)
+
 number = number_sorting(5, 6, 7, 8, 3, 6, 9, 20)
 print(number)
